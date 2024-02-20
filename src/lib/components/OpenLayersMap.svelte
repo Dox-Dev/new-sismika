602d2ae950502bf9091dac743bc1bb2d77fba2fa
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-02-20 19:10:21 +0800

    feat: make map adjust to light and dark mode

--- src/lib/components/OpenLayersMap.svelte
+++ src/lib/components/OpenLayersMap.svelte
@@ -5,23 +5,26 @@
     import View from 'ol/View';
     import TileLayer from 'ol/layer/Tile';
     import OSM from 'ol/source/OSM';
+    import XYZ from 'ol/source/XYZ';
   
     // Used to convert from [longitude, latitude] to 
     // a coordinate system OpenLayers can read
     // https://stackoverflow.com/questions/27820784/openlayers-3-center-map
     import { fromLonLat } from 'ol/proj';
 
+    const sourceOSM = new XYZ();
     let mapElement;
-  
+    // Initialize the mount as well as the map
     onMount(() => {
       new Map({
         target: mapElement,
         layers: [
           new TileLayer({
             // https://github.com/CartoDB/basemap-styles
-            source: new OSM({
-              url: 'https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-            }),
+            //source: new OSM({
+            //  url: 'https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+            //}),
+            source: sourceOSM,
           }),
         ],
         view: new View({
@@ -30,6 +33,15 @@
         }),
       });
     });
+
+    import { modeCurrent } from '@skeletonlabs/skeleton';;
+
+    let themeURL;
+    $: themeURL = ($modeCurrent)?
+    'https://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png':
+    'https://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
+
+    $: sourceOSM.setUrl(themeURL);
   </script>
   
   <style>
