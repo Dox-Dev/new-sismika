be2072d279cc1106c9929d99a73ca3e721564c85
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-02-23 13:21:54 +0800

    fix(`util.ts`): remove `id` field

---
+++ models/src/util.ts
@@ -0,0 +1,9 @@
+
+import { z } from 'zod';
+
+export const CoordinatesSchema = z.object({
+    long: z.number(),
+    lat: z.number()
+});
+
+export type Coordinates = z.infer<typeof CoordinatesSchema>;
