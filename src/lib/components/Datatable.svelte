1fc4688d59459969b6d9847c08291442decb26da
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-02-24 17:50:21 +0800

    feat: implement datatable under map

---
+++ src/lib/components/Datatable.svelte
@@ -0,0 +1,56 @@
+<script>
+	//Import local datatable components
+	import ThSort from '$lib/components/client/ThSort.svelte';
+	import ThFilter from '$lib/components/client/ThFilter.svelte';
+	import Search from '$lib/components/client/Search.svelte';
+	import RowsPerPage from '$lib/components/client/RowsPerPage.svelte';
+	import RowCount from '$lib/components/client/RowCount.svelte';
+	import Pagination from '$lib/components/client/Pagination.svelte';
+
+	//Import handler from SSD
+	import { DataHandler } from '@vincjo/datatables';
+
+    //Load local data
+	export let data;
+
+	//Init data handler - CLIENT
+	const handler = new DataHandler(data, { rowsPerPage: 5 });
+	const rows = handler.getRows();
+</script>
+
+<div class=" overflow-x-auto space-y-4">
+	<!-- Header -->
+	<header class="flex justify-between gap-4">
+		<Search {handler} />
+		<RowsPerPage {handler} />
+	</header>
+	<!-- Table -->
+	<table class="table table-hover table-compact w-full table-auto">
+		<thead>
+			<tr>
+				<ThSort {handler} orderBy="first_name">First name</ThSort>
+				<ThSort {handler} orderBy="last_name">Last name</ThSort>
+				<ThSort {handler} orderBy="email">Email</ThSort>
+			</tr>
+			<tr>
+				<ThFilter {handler} filterBy="first_name" />
+				<ThFilter {handler} filterBy="last_name" />
+				<ThFilter {handler} filterBy="email" />
+			</tr>
+		</thead>
+		<tbody>
+			{#each $rows as row}
+				<tr>
+					<td>{row.first_name}</td>
+					<td>{row.last_name}</td>
+					<td>{row.email}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+	<!-- Footer -->
+	<footer class="flex justify-between">
+		<RowCount {handler} />
+		<Pagination {handler} />
+	</footer>
+</div>
