4e22396aac62014ffff54e95c934bdb658e88009
Marc Macaraeg <mgcmacaraeg@gmail.com>
2024-02-26 11:11:04 +0800

    feat: implement better preliminary display for events

--- src/routes/earthquake/[id]/+page.svelte
+++ src/routes/earthquake/[id]/+page.svelte
@@ -2,4 +2,4 @@
     export let data;
     $: ({ coord:{long, lat}, depth, _id, li, time } = data);
 </script>
-<h1>{long} {lat}, {depth}, {_id}, {li}, {time}</h1>
+<h1>Longitude: {long} degrees, Latitude: {lat} degrees, Depth:{depth}km, ID: {_id}, Local Intensity: {li}, Time: {time}</h1>
