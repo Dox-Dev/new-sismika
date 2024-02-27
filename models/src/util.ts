a4b839d21232605717c86c88b0cc6548a02f8513
Marc Macaraeg <mgcmacaraeg@gmail.com>
2024-02-27 12:17:58 +0800

    feat: implement auth models

--- models/src/util.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const CoordinatesSchema = z.object({
-    long: z.number(),
-    lat: z.number()
-});
-
-export type Coordinates = z.infer<typeof CoordinatesSchema>;
---
+++ src/lib/model/src/auth.ts
@@ -0,0 +1,24 @@
+import { ObjectIDSchema } from "./util";
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+    _id: ObjectIDSchema,
+    email: z.string().email(),
+    username: z.string(),
+    givenName: z.string(),
+    familyName: z.string(),
+    picture: z.string().url(),
+    local: z.string()
+})
+
+export type User = z.infer<typeof UserSchema>
+
+export const SessionSchema = z.object({
+    _id: ObjectIDSchema,
+    userId: ObjectIDSchema,
+    createdAt: z.string().datetime(),
+    expiresAt: z.string().datetime()
+})
+
+export type Session = z.infer<typeof SessionSchema>
+
