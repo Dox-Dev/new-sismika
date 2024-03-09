3ca690ac5ef1e40061ad1a9403571209ff01d6c0
Marc <mgc.macaraeg@gmail.com>
2024-03-09 17:43:06 +0800

    feat: add env files for oauth and mongodb

---
+++ src/lib/model/src/env/oauth.ts
@@ -0,0 +1,12 @@
+import { env } from "$env/dynamic/private";
+import { ok } from "assert";
+
+const { GOOGLE_ID, GOOGLE_SECRET, OAUTH_REDIRECT } = env;
+ok(GOOGLE_ID);
+ok(GOOGLE_SECRET);
+
+export default {
+    GOOGLE_ID,
+    GOOGLE_SECRET,
+    OAUTH_REDIRECT: OAUTH_REDIRECT || 'http://127.0.0.1/auth/callback',
+};
