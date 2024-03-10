7d84c3cfec9b4a1ea57a431a1395984bd103f347
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-10 11:44:25 +0800

    feat(`seismic/submit`): allow users to submit entries in the table

---
+++ src/routes/seismic/submit/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    // export const EvacCenterSchema = z.object({
+	//     _id: ObjectIDSchema,
+	//     name: z.string(),
+	//     coord: CoordinatesSchema
+    // });
+</script>
+
+<h1 class="h1">Seismic Station Submission</h1>
+<!-- <h1>Evacuation Center Submission</h1> -->
+<br/>
+<!-- To connect with certain values, use bind:value={variableName} in the input header-->
+<label class="label">
+	<span class="h3">ID</span>
+	<input class="input" type="text" placeholder="Input the seismic station ID here" />
+</label>
+<br/>
+<label class="label">
+	<span class="h3">Code</span>
+    <input class="input" type="text" placeholder="Input the evacuation center code here" />
+</label>
+<br/>
+<label class="label">
+	<span class="h3">Name</span>
+    <input class="input" type="text" placeholder="Input the evacuation center code here" />
+</label>
+<br/>
+<label class="label">
+	<span class="h3">Type</span>
+    <input class="input" type="text" placeholder="Input the evacuation center code here" />
+</label>
+<br/>
+
+<span class="h3">Coordinates</span>
+<div class="input-group input-group-divider grid-cols-[1fr_1fr]">
+    <input type="text" placeholder="Longitude" />
+    <input type="text" placeholder="Latitude" />
+</div>
+
+<br>
+<button type="button" class="btn btn-sm variant-filled">Submit</button>
+
