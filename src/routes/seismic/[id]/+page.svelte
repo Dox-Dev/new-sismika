e60c5a3a807a148e78b6e93eef5fa3f0ae5c36b8
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-10 11:42:59 +0800

    feat(`seismic/[id]`): show individual entry

---
+++ src/routes/seismic/[id]/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+    //export let data;
+
+	// remove dummy data later when exporting data from endpoints
+	// $: ({ coord:{long, lat}, depth, _id, li, time } = data);
+	const row = {
+		_id: "1",
+		code: "123",
+		name: "evaccenter",
+		type: "abc",
+		coord: {
+			long: 111,
+			lat: 22,
+		}
+	}
+</script>
+
+<!-- Responsive Container (recommended) -->
+<div class="table-container">
+	<!-- Native Table Element -->
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Seismic Station ID</th>
+				<th>Code</th>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Longitude</th>
+				<th>Latitude</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- {#each tableArr as row, i} -->
+				<tr>
+					<td>{row._id}</td>
+					<td>{row.code}</td>
+					<td>{row.name}</td>
+					<td>{row.type}</td>
+					<td>{row.coord.long}</td>
+					<td>{row.coord.lat}</td>
+				</tr>
+			<!-- {/each} -->
+		</tbody>
+	</table>
+</div>
