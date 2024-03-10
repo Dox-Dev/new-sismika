e4124358ec6224c45254d08fd60a589c51f9439f
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-10 11:42:12 +0800

    feat(`seismic`): add `Datatable`

---
+++ src/routes/seismic/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Datatable from '$lib/components/Datatable-seismic.svelte';
+    export let data;
+</script>
+
+<main>
+    <div>
+        <Datatable {data}/>
+    </div>
+
+    <div>
+        Anything missing? Tell us!
+        <a href="/seismic/submit">
+        <button type="button" class="btn btn-sm variant-filled">
+            Submit
+        </button>
+        </a>
+    </div>
+</main>
