7dcf3318d9b69c2e8a8223cd64b4b00c82dc167d
Marc Macaraeg <mgcmacaraeg@gmail.com>
2024-03-16 10:12:12 +0800

    fix: erroneous pagehandler for seismic stations

--- src/routes/seismic/[id]/+page.server.ts
+++ src/routes/seismic/[id]/+page.server.ts
@@ -1,16 +1,15 @@
-import { getAllStationData } from "$lib/server/database";
+import { getAllStationData, getStationData } from "$lib/server/database";
 import { error } from "@sveltejs/kit";
 import { StatusCodes } from "http-status-codes";
+import { ObjectId } from "mongodb";
 
-export async function load() {
-    const res = await getAllStationData();
+export async function load({params: {id}}) {
+    const query = ObjectId.createFromHexString(id)
+    const res = await getStationData(query);
+
     if (res === false) error(StatusCodes.NOT_FOUND);
+    res._id = res._id?.toString()
 
-    res.forEach(el => {
-        el._id = el._id?.toString()
-    })
-
-    const data = { stationData: res};
-    return data;
+    return {selectedStation: res}
 }
 
