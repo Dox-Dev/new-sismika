29c0867dd4f94f5137a457972b573c09e06923fb
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-16 11:24:08 +0800

    feat(`submit`): enclose submission labels and buttons in HTML forms

--- src/routes/earthquake/submit/+page.svelte
+++ src/routes/earthquake/submit/+page.svelte
@@ -18,33 +18,40 @@ export const EarthquakeEventSchema = z.object({
 <!-- <h1>Evacuation Center Submission</h1> -->
 <br />
 <!-- To connect with certain values, use bind:value={variableName} in the input header-->
-<label class="label">
-	<span class="h3">ID</span>
-	<input class="input" type="text" placeholder="Input the earthquake ID here" />
-</label>
-<br />
-<label class="label">
-	<span class="h3">Time</span>
-	<input class="input" type="text" placeholder="Input the time the earthquake happened here" />
-</label>
-<br />
+<form
+use:enhance
+method="POST"
+enctype="application/x-www-form-urlencoded"
+class="card space-y-4 p-4"
+>
+	<label class="label">
+		<span class="h3">ID</span>
+		<input class="input" type="text" placeholder="Input the earthquake ID here" />
+	</label>
+	<br />
+	<label class="label">
+		<span class="h3">Time</span>
+		<input class="input" type="text" placeholder="Input the time the earthquake happened here" />
+	</label>
+	<br />
 
-<span class="h3">Local Intensity</span>
-<textarea class="textarea" rows="4" placeholder="Local Intensity" />
-<br />
+	<span class="h3">Local Intensity</span>
+	<textarea class="textarea" rows="4" placeholder="Local Intensity" />
+	<br />
 
-<span class="h3">Coordinates</span>
-<div class="input-group input-group-divider grid-cols-[1fr_1fr]">
-	<input type="text" placeholder="Longitude" />
-	<input type="text" placeholder="Latitude" />
-</div>
+	<span class="h3">Coordinates</span>
+	<div class="input-group input-group-divider grid-cols-[1fr_1fr]">
+		<input type="text" placeholder="Longitude" />
+		<input type="text" placeholder="Latitude" />
+	</div>
 
-<span class="h3">Magnitudes</span>
-<div class="input-group input-group-divider grid-cols-[1fr_1fr_1fr]">
-	<input type="text" placeholder="Moment Magnitude" />
-	<input type="text" placeholder="Body-Wave Magnitude" />
-	<input type="text" placeholder="Surface Wave Magnitude" />
-</div>
+	<span class="h3">Magnitudes</span>
+	<div class="input-group input-group-divider grid-cols-[1fr_1fr_1fr]">
+		<input type="text" placeholder="Moment Magnitude" />
+		<input type="text" placeholder="Body-Wave Magnitude" />
+		<input type="text" placeholder="Surface Wave Magnitude" />
+	</div>
 
-<br />
-<button type="button" class="btn btn-sm variant-filled">Submit</button>
+	<br />
+	<button type="button" class="btn btn-sm variant-filled">Submit</button>
+</form>
--- src/routes/seismic/submit/+page.svelte
+++ src/routes/seismic/submit/+page.svelte
@@ -10,32 +10,39 @@
 <!-- <h1>Evacuation Center Submission</h1> -->
 <br />
 <!-- To connect with certain values, use bind:value={variableName} in the input header-->
-<label class="label">
-	<span class="h3">ID</span>
-	<input class="input" type="text" placeholder="Input the seismic station ID here" />
-</label>
-<br />
-<label class="label">
-	<span class="h3">Code</span>
-	<input class="input" type="text" placeholder="Input the evacuation center code here" />
-</label>
-<br />
-<label class="label">
-	<span class="h3">Name</span>
-	<input class="input" type="text" placeholder="Input the evacuation center code here" />
-</label>
-<br />
-<label class="label">
-	<span class="h3">Type</span>
-	<input class="input" type="text" placeholder="Input the evacuation center code here" />
-</label>
-<br />
-
-<span class="h3">Coordinates</span>
-<div class="input-group input-group-divider grid-cols-[1fr_1fr]">
-	<input type="text" placeholder="Longitude" />
-	<input type="text" placeholder="Latitude" />
-</div>
-
-<br />
-<button type="button" class="btn btn-sm variant-filled">Submit</button>
+<form
+use:enhance
+method="POST"
+enctype="application/x-www-form-urlencoded"
+class="card space-y-4 p-4"
+>
+	<label class="label">
+		<span class="h3">ID</span>
+		<input class="input" type="text" placeholder="Input the seismic station ID here" />
+	</label>
+	<br />
+	<label class="label">
+		<span class="h3">Code</span>
+		<input class="input" type="text" placeholder="Input the evacuation center code here" />
+	</label>
+	<br />
+	<label class="label">
+		<span class="h3">Name</span>
+		<input class="input" type="text" placeholder="Input the evacuation center code here" />
+	</label>
+	<br />
+	<label class="label">
+		<span class="h3">Type</span>
+		<input class="input" type="text" placeholder="Input the evacuation center code here" />
+	</label>
+	<br />
+	
+	<span class="h3">Coordinates</span>
+	<div class="input-group input-group-divider grid-cols-[1fr_1fr]">
+		<input type="text" placeholder="Longitude" />
+		<input type="text" placeholder="Latitude" />
+	</div>
+	
+	<br />
+	<button type="button" class="btn btn-sm variant-filled">Submit</button>
+</form>
