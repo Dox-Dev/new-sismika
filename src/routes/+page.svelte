b4bcef1c87e8d26f835236510b0d92110af2bca5
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-16 19:52:27 +0800

    feat(`homepage`): add description of project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,19 @@
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
+<script>
+	let innerWidth = 0
+	let innerHeight = 0
+	
+	$: condition = innerWidth*1.33 <= innerHeight
+</script>
+
 <svelte:head>
 	<title>Sismika</title>
 	<meta name="description" content="Sismika interactive earthquake visualizer" />
 </svelte:head>
+
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <!--
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -22,7 +31,7 @@
 	</div>
 </div>
 -->
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full v-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<div>
 			<h1 class="h1 text-center">
@@ -30,7 +39,74 @@
 				SISMIKA
 				</span>
 			</h1>
-			<h2 class="text-2xl">An Interactive Earthquake Visualizer</h2>
+			<h2 class="text-2xl text-center mb-12">An Interactive Earthquake Visualizer</h2>
+			
+			{#if innerWidth*1.33 <= innerHeight}
+				<p class="mx-6 mb-6">
+				Introducing our groundbreaking 
+				Seismic Visualizer 					
+				<span class="text-primary-500">
+					Sismika</span>,  
+				a cutting-edge tool developed by 
+				<span class="text-primary-500">
+					Marc Macaraeg</span>
+				and 
+				<span class="text-primary-500">
+					Ellis Caluag</span>
+				that revolutionizes the way we 
+				interpret seismic data.
+				</p>
+
+				<p class="mx-6 mb-6">
+				Tailored for seismic stations and 
+				events, our visualizer is a
+				<span class="text-primary-500">
+					comprehensive solution designed 
+					to enhance your understanding of 
+					seismic activities</span>,
+				including 
+				earthquakes and volcanic events.
+				</p>
+
+				<p class="mx-6 mb-6">
+				Should you need any assistance, or
+				desire to report bugs, please do feel
+				free to contact or message us in our
+				socials shown directly below. We'll 
+				be happy to accommodate you.
+				</p>
+			{:else}
+				<p class="mx-12">
+					Introducing our groundbreaking 
+					Seismic Visualizer 
+					<span class="text-primary-500">
+						Sismika</span>, 
+					a cutting-edge tool developed by 
+					<span class="text-primary-500">
+						Marc Macaraeg</span>
+					and 
+					<span class="text-primary-500">
+						Ellis Caluag
+					</span> 
+					that revolutionizes the way we 
+					interpret seismic data.
+					
+					Tailored for seismic stations and 
+					events, our visualizer is a
+					<span class="text-primary-500">
+						comprehensive solution designed 
+						to enhance your understanding of 
+						seismic activities</span>,
+					including 
+					earthquakes and volcanic events.
+	
+					Should you need any assistance, or
+					desire to report bugs, please do feel
+					free to contact or message us in our
+					socials shown directly below. We'll 
+					be happy to accommodate you.
+				</p>
+			{/if}
 		</div>
 	</div>
 </div>
