fe0da6ce4cd03943a63d685f218081c265abc7d0
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-16 18:32:05 +0800

    style: represent Sismika in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,10 @@
 	<meta name="description" content="Sismika interactive earthquake visualizer" />
 </svelte:head>
 
+<!--
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Welcome to Sismika!</h1>
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
@@ -20,19 +21,37 @@
 		</ul>
 	</div>
 </div>
-
+-->
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<div>
+			<h1 class="h1 text-center">
+				<span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone text-8xl">
+				SISMIKA
+				</span>
+			</h1>
+			<h2 class="text-2xl">An Interactive Earthquake Visualizer</h2>
+		</div>
+	</div>
+</div>
+<!--
 <section>
 	<h1>Dashboard</h1>
 	<div class="mini-windows">
 		<div class="graph-container">
+			-->
 			<!-- <MiniChart /> -->
+			<!--
 			<a href="/chart"><button>View full graph</button></a>
 		</div>
 		<div class="logs-and-button">
 			<div class="logs-container">
+				-->
 				<!-- <MiniLogs /> -->
+				<!--
 			</div>
 			<a href="/eventlogs"><button>View logs</button></a>
 		</div>
 	</div>
 </section>
+-->
