88f0a32c04310b82f70a5aa26f1e1f4f92f3129d
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-16 21:15:12 +0800

    chore(`homepage`): remove commented-out code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,21 +16,6 @@
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<!--
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Welcome to Sismika!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
--->
 <div class="container h-full v-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<div>
@@ -118,24 +103,3 @@
 		</center>
 	</div>
 </div>
-<!--
-<section>
-	<h1>Dashboard</h1>
-	<div class="mini-windows">
-		<div class="graph-container">
-			-->
-			<!-- <MiniChart /> -->
-			<!--
-			<a href="/chart"><button>View full graph</button></a>
-		</div>
-		<div class="logs-and-button">
-			<div class="logs-container">
-				-->
-				<!-- <MiniLogs /> -->
-				<!--
-			</div>
-			<a href="/eventlogs"><button>View logs</button></a>
-		</div>
-	</div>
-</section>
--->
