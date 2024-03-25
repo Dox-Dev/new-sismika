3826bcb5c382cbcbe33d00e97b7e66a720d333eb
Marc <mgc.macaraeg@gmail.com>
2024-03-25 11:06:10 +0800

    feat: update models for posts again

--- src/lib/model/src/posts.ts
+++ src/lib/model/src/posts.ts
@@ -7,31 +7,18 @@ export enum MEDIATYPE {
     Image,
     Video,
     Article,  
+    Comment,
 }
 
-export const PostBase = z.object({
+export const MediaSchema = z.object({
     _id: ObjectIDSchema,
     equakeId: z.instanceof(ObjectId),
     type: z.number(),
-    time: z.string().datetime()
-})
-
-export const MediaSchema = PostBase.extend({
-    url: z.string().url()
+    time: z.string().datetime(),
+    url: z.string().url().optional(),
+    content: z.string().min(1).optional()
 })
 
 export type Media = z.infer<typeof MediaSchema>
-
-export const ArticleSchema = PostBase.extend({
-    content: z.string().min(1)
-})
-
-export type Article = z.infer<typeof ArticleSchema>
-
-export const CommentSchema = z.object({
-    _id: ObjectIDSchema,
-    earthquakeId: z.instanceof(ObjectId),
-    authodId: GoogleUserId,
-    content: z.string().min(1)
-})
+export const MediaArraySchema = z.array(MediaSchema)
 
