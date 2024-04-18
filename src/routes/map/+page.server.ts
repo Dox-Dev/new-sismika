a303aa1f3400bbf640b60440a0f18485b99013e1
Marc Macaraeg <mgcmacaraeg@gmail.com>
2024-04-18 18:46:33 +0800

    fix: map view to follow new endpoints

--- src/routes/map/+page.server.ts
+++ src/routes/map/+page.server.ts
@@ -1,13 +1,9 @@
 import { getAllEarthquakeData, getAllEvacData, getAllStationData } from '$lib/server/database';
-import { error } from '@sveltejs/kit';
-import { StatusCodes } from 'http-status-codes';
 
 export async function load() {
 	const equakeData = await getAllEarthquakeData();
 	const evacData = await getAllEvacData();
 	const stationData = await getAllStationData();
 
-	if ([equakeData, evacData, stationData].every((el) => el === false)) error(StatusCodes.NOT_FOUND);
-
-	return { equake: equakeData.equakes, evac: evacData, station: stationData.stations };
+	return { equake: equakeData.equakes, evac: evacData.evacs, station: stationData.stations };
 }
