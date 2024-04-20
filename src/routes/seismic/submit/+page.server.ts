2b42f6f7e75135d708b3c3a682caf497af528140
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-04-20 17:50:11 +0800

    feat(`seismic/submit`): allow authorized users to submit entries

--- src/routes/seismic/submit/+page.server.ts
+++ src/routes/seismic/submit/+page.server.ts
@@ -1,3 +1,4 @@
+/*
 import { StationSchema } from '$lib/model/src/station.js';
 import { Permission } from '$lib/model/src/user.js';
 import { addStationData, getUserFromSession } from '$lib/server/database/index.js';
@@ -50,3 +51,65 @@ export const actions = {
 		throw error(StatusCodes.NOT_FOUND);
 	}
 };
+*/
+
+import { fail, redirect } from '@sveltejs/kit';
+import { StatusCodes } from 'http-status-codes';
+import type { Actions } from './$types';
+import { addStationData, getUserFromSession } from '$lib/server/database';
+import { Permission } from '$lib/model/src/user';
+import { StationSchema } from '$lib/model/src/station';
+
+export const actions = {
+	async default({ cookies, request }) {
+		const form = await request.formData();
+
+		const code = form.get('code')?.toString();
+		if (code === undefined) return fail(StatusCodes.BAD_REQUEST, { missing: true });
+
+		const name = form.get('name')?.toString();
+		if (name === undefined) return fail(StatusCodes.BAD_REQUEST, { code, missing: true });
+
+		const type = form.get('type')?.toString();
+		if (name === undefined) return fail(StatusCodes.BAD_REQUEST, { code, name, missing: true });
+
+		const long = form.get('long')?.toString();
+		if (long === undefined) return fail(StatusCodes.BAD_REQUEST, { code, name, type, missing: true });
+
+		const lat = form.get('lat')?.toString();
+		if (lat === undefined) return fail(StatusCodes.BAD_REQUEST, { code, name, type, long, missing: true });
+
+		console.log("reaches before trycatch");
+		const sid = cookies.get('sid');
+		if (!sid) return fail(StatusCodes.UNAUTHORIZED, { code, name, type, long, lat, authFail: true });
+		// A check can be done to see if a session exists.
+		
+		const user = await getUserFromSession(sid);
+		if (user === false)
+			return fail(StatusCodes.UNAUTHORIZED, { code, name, type, long, lat, authFail: true });
+		if (user.permission < Permission.RESEARCHER)
+			return fail(StatusCodes.UNAUTHORIZED, { code, name, type, long, lat, noPerms: true });
+
+		const insert = {
+			code,
+			name,
+			type,
+			coord: {
+				type: 'Point',
+				coordinates: [parseFloat(long), parseFloat(lat)]
+			},
+		};
+		
+		try {
+			const insertValidated = StationSchema.parse(insert);
+			const result = await addStationData(insertValidated);
+			const resId = result.toString();
+			if (typeof result === 'object') {
+				return redirect(StatusCodes.MOVED_TEMPORARILY, `/seismic/${resId}`);
+			}
+			return fail(StatusCodes.BAD_REQUEST, { long, lat, parseFail: true });
+		} catch (err) {
+			throw err;
+		}
+	}
+} satisfies Actions;
