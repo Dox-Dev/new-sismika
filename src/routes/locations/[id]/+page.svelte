934047e1a4a6679c53d99e9be5a2dee009e03653
Marc <mgc.macaraeg@gmail.com>
2024-04-22 14:48:02 +0800

    feat: implement locations page

--- src/routes/locations/[id]/+page.svelte
+++ src/routes/locations/[id]/+page.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
-	import OpenLayersMap from "../../map/OpenLayersMap.svelte";
+	import EarthquakeCard from "$lib/components/ui/EarthquakeCard.svelte";
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
+    import LocationCard from "$lib/components/ui/LocationCard.svelte";
+	import VerticalContainer from "$lib/components/ui/containers/VerticalContainer.svelte";
+    import OpenLayersMap from "../../map/OpenLayersMap.svelte";
+	import type { PageData } from "./$types";
 
-    export let data;
+    export let data: PageData;
+
+    $: currentLocation = data.location;
+
 </script>
+<div class="card flex-row">
+    <article>   
+        <LocationCard {...data.location}/>
+    </article>
+    <div>
+    <Accordion>
+        <AccordionItem>
+            <svelte:fragment slot="summary"><i class="fa-regular fa-map"></i>Map</svelte:fragment>
+            <svelte:fragment slot="content">
+                <OpenLayersMap {data} centerCoord={data.location.coord?.coordinates} zoom={9}/>
+            </svelte:fragment>
+        </AccordionItem>
+        <AccordionItem open>
+            <svelte:fragment slot="summary"><i class="fa-solid fa-house-crack"></i>Earthquakes</svelte:fragment>
+            <svelte:fragment slot="content">
+                <VerticalContainer>
+                    {#each data.equake as {mw, _id, time}}
+                        <EarthquakeCard magnitude={mw} equakeId={_id} dateTime={time}/>
+                    {/each}
+                </VerticalContainer>
+            </svelte:fragment>
+        </AccordionItem>
+    </Accordion>
 
-<OpenLayersMap {data}/>
+
+</div>
+</div>
