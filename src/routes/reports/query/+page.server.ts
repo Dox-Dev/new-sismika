55e051190f082346f3e9e3b1ccda436b3cf9bc99
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-05-03 18:41:53 +0800

    feat(`reports`): copy over submission form template from `earthquakes`

---
+++ src/routes/reports/query/+page.server.ts
@@ -0,0 +1,74 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import { StatusCodes } from 'http-status-codes';
+import type { Actions } from './$types';
+//import { addEarthquakeData, getUserFromSession } from '$lib/server/database';
+import { getUserFromSession } from '$lib/server/database';
+import { Permission } from '$lib/model/src/user';
+import { parseOrZero } from '$lib/model/src/util';
+import { EarthquakeFilteringSchema } from '$lib/model/src/event';
+
+export const actions = {
+	async default({ cookies, request }) {
+		const form = await request.formData();
+
+		const time = form.get('time')?.toString();
+		if (time === undefined) return fail(StatusCodes.BAD_REQUEST, { missing: true });
+
+		const long = form.get('long')?.toString();
+		if (long === undefined) return fail(StatusCodes.BAD_REQUEST, { time, missing: true });
+
+		const lat = form.get('lat')?.toString();
+		if (lat === undefined) return fail(StatusCodes.BAD_REQUEST, { time, long, missing: true });
+
+		const depth = form.get('depth');
+		if (depth === null || depth instanceof File)
+			return fail(StatusCodes.BAD_REQUEST, { time, long, lat, missing: true });
+		const mi = form.get('mi')?.toString();
+		const mb = form.get('mb')?.toString();
+		const ms = form.get('ms')?.toString();
+		const mw = form.get('mw')?.toString();
+		if (mi === undefined && mb === undefined && ms === undefined && mw === undefined)
+			return fail(StatusCodes.BAD_REQUEST, { time, long, lat, depth, missing: true });
+
+		const li = form.get('li')?.toString();
+		if (li === undefined)
+			return fail(StatusCodes.BAD_REQUEST, { time, long, lat, depth, mw, missing: true });
+
+		const sid = cookies.get('sid');
+		if (!sid) return fail(StatusCodes.UNAUTHORIZED, { time, long, lat, depth, mw, authFail: true });
+		// A check can be done to see if a session exists.
+
+		const user = await getUserFromSession(sid);
+		if (user === false)
+			return fail(StatusCodes.UNAUTHORIZED, { time, long, lat, depth, mw, authFail: true });
+		if (user.permission < Permission.RESEARCHER)
+			return fail(StatusCodes.UNAUTHORIZED, { time, long, lat, depth, mw, noPerms: true });
+
+		const insert = {
+			time: new Date(time).toISOString(),
+			coord: {
+				type: 'Point',
+				coordinates: [parseFloat(long), parseFloat(lat)]
+			},
+			depth: parseInt(depth),
+			mi: parseOrZero(mi),
+			mb: parseOrZero(mb),
+			ms: parseOrZero(ms),
+			mw: parseOrZero(mw),
+			li: li ?? '',
+			title: ''
+		};
+
+		try {
+			const insertValidated = EarthquakeEventSchema.parse(insert);
+			const result = await addEarthquakeData(insertValidated);
+			const resId = result.toString();
+			if (typeof result === 'object') {
+				return redirect(StatusCodes.MOVED_TEMPORARILY, `/earthquake/${resId}`);
+			}
+			return fail(StatusCodes.BAD_REQUEST, { time, long, lat, depth, mw, li, parseFail: true });
+		} catch (err) {
+			throw err;
+		}
+	}
+} satisfies Actions;
---
+++ src/routes/reports/query/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import LoginUserPromptError from '$lib/components/ui/LoginUserPromptError.svelte';
+
+	export let form;
+	let entryForm: HTMLFormElement;
+</script>
+
+<h1 class="h1">Earthquake Entry Submission</h1>
+<hr />
+{#if form?.authFail || form?.noPerms}
+	<LoginUserPromptError />
+{/if}
+<form
+	method="POST"
+	enctype="application/x-www-form-urlencoded"
+	class="flex-col flex card space-y-4 p4"
+	bind:this={entryForm}
+>
+	<article class="card flex-row m-4 p-1 space-y-3">
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Datetime</div>
+			<input
+				name="time"
+				title="Input Datetime for Earthquake Event"
+				type="datetime-local"
+				value={form?.time ?? ''}
+			/>
+		</div>
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Longitude</div>
+			<input
+				class:input-error={!form?.long && form?.missing}
+				type="number"
+				name="long"
+				title="Longitude"
+				step="any"
+				value={form?.long ?? ''}
+			/>
+			<div class="input-group-shim">°</div>
+		</div>
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Latitude</div>
+			<input type="number" name="lat" title="Latitude" step="any" value={form?.lat ?? ''} />
+			<div class="input-group-shim">°</div>
+		</div>
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Depth</div>
+			<input type="number" name="depth" title="Depth" step="any" value={form?.depth ?? ''} />
+			<div class="input-group-shim">km</div>
+		</div>
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Moment Magnitude</div>
+			<input type="number" name="mw" title="Magnitude" step="any" value={form?.mw ?? ''} />
+			<div class="input-group-shim">mw</div>
+		</div>
+		<textarea
+			name="li"
+			class="textarea"
+			rows="4"
+			placeholder="Enter local intensities."
+			value={form?.li ?? ''}
+		/>
+	</article>
+	<button type="button" class="btn btn-sm variant-filled" on:click={() => entryForm.requestSubmit()}
+		>Submit</button
+	>
+</form>
