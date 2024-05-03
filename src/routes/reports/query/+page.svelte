87cf21a3551d8a9eb3a5e17a7fe75932c99a4947
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-05-04 04:55:23 +0800

    feat(`reports/query`): ask for needed report info

--- src/routes/reports/query/+page.svelte
+++ src/routes/reports/query/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-	import LoginUserPromptError from '$lib/components/ui/LoginUserPromptError.svelte';
+	import { RangeSlider } from '@skeletonlabs/skeleton';
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
+	import LoginUserPromptError from '$lib/components/ui/LoginUserPromptError.svelte';
+	
 	export let form;
 	let entryForm: HTMLFormElement;
+
+	let isBoundingBox:boolean;
+	$: isBoundingBox = true;
 </script>
 
 <h1 class="h1">Earthquake Entry Submission</h1>
@@ -18,48 +24,119 @@
 >
 	<article class="card flex-row m-4 p-1 space-y-3">
 		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-			<div class="input-group-shim">Datetime</div>
-			<input
-				name="time"
-				title="Input Datetime for Earthquake Event"
-				type="datetime-local"
-				value={form?.time ?? ''}
-			/>
-		</div>
-		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-			<div class="input-group-shim">Longitude</div>
-			<input
-				class:input-error={!form?.long && form?.missing}
-				type="number"
-				name="long"
-				title="Longitude"
-				step="any"
-				value={form?.long ?? ''}
-			/>
-			<div class="input-group-shim">°</div>
-		</div>
-		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-			<div class="input-group-shim">Latitude</div>
-			<input type="number" name="lat" title="Latitude" step="any" value={form?.lat ?? ''} />
-			<div class="input-group-shim">°</div>
-		</div>
-		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-			<div class="input-group-shim">Depth</div>
-			<input type="number" name="depth" title="Depth" step="any" value={form?.depth ?? ''} />
+			<div class="input-group-shim">Maximum Depth</div>
+			<input type="number" name="depth" title="maxDepth" step="any" value={form?.maxDepth ?? ''} />
 			<div class="input-group-shim">km</div>
 		</div>
+
 		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
-			<div class="input-group-shim">Moment Magnitude</div>
-			<input type="number" name="mw" title="Magnitude" step="any" value={form?.mw ?? ''} />
-			<div class="input-group-shim">mw</div>
+			<div class="input-group-shim">Minimum Depth</div>
+			<input type="number" name="depth" title="minDepth" step="any" value={form?.minDepth ?? ''} />
+			<div class="input-group-shim">km</div>
 		</div>
-		<textarea
-			name="li"
-			class="textarea"
-			rows="4"
-			placeholder="Enter local intensities."
-			value={form?.li ?? ''}
-		/>
+
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Maximum Magnitude</div>
+			<input type="number" min={0} name="maxIntensity" title="maxIntensity" step="any" value={form?.maxIntensity ?? ''} />
+			<!-- <div class="input-group-shim">km</div> -->
+		</div>
+
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Minimum Magnitude</div>
+			<input type="number" min={0} name="minIntensity" title="minIntensity" step="any" value={form?.minIntensity ?? ''} />
+			<!-- <div class="input-group-shim">km</div> -->
+		</div>
+
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Maximum Time</div>
+			<input
+				name="maxTime"
+				title="Input Datetime for Earthquake Event"
+				type="datetime-local"
+				value={form?.maxTime ?? ''}
+			/>
+		</div>
+
+		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+			<div class="input-group-shim">Minimum Time</div>
+			<input
+				name="minTime"
+				title="Input Datetime for Earthquake Event"
+				type="datetime-local"
+				value={form?.minTime ?? ''}
+			/>
+		</div>
+
+		
+		<div class="space-y-2">
+			<label class="flex items-center space-x-2">
+				<input class="radio" bind:group={isBoundingBox} type="radio" checked name="radio-direct-1" value={true}/>
+				<p>Bounding Box - Give the 4 corner coordinates of affected areas</p>
+			</label>
+			<label class="flex items-center space-x-2">
+				<input class="radio" bind:group={isBoundingBox} type="radio" name="radio-direct-2" value={false} />
+				<p>Radial Bounding - Give the center and radius to encapsulate the affected areas</p>
+			</label>
+		</div>
+
+					
+
+		{#if isBoundingBox}
+			<h2 class="h2">Bounding Box</h2>
+			<div class="input-group input-group-divider grid-cols-[0.6fr_1fr_1fr]">
+				<div class="input-group-shim">Top Left Coordinate</div>
+				<input type="number" name="TL_long" title="Longitude" placeholder="Longitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[0][0] ?? ''} />
+				<input type="number" name="TL_lat" title="Latitude" placeholder="Latitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[0][1] ?? ''} />
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[0.6fr_1fr_1fr]">
+				<div class="input-group-shim">Top Right Coordinate</div>
+				<input type="number" name="TR_long" title="Longitude" placeholder="Longitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[1][0] ?? ''} />
+				<input type="number" name="TR_lat" title="Latitude" placeholder="Latitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[1][1] ?? ''} />
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[0.6fr_1fr_1fr]">
+				<div class="input-group-shim">Bottom Left Coordinate</div>
+				<input type="number" name="BL_long" title="Longitude" placeholder="Longitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[2][0] ?? ''} />
+				<input type="number" name="BL_lat" title="Latitude" placeholder="Latitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[2][1] ?? ''} />
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[0.6fr_1fr_1fr]">
+				<div class="input-group-shim">Bottom Right Coordinate</div>
+				<input type="number" name="BR_long" title="Longitude" placeholder="Longitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[3][0] ?? ''} />
+				<input type="number" name="BR_lat" title="Latitude" placeholder="Latitude in °" step="any" 
+					value={form?.geographicBound.BoundingBoxSchema.coordinates[3][1] ?? ''} />
+			</div>
+		{:else}
+			<h2 class="h2">Radial Bounding</h2>
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_1fr]">
+				<div class="input-group-shim">Center Coordinates</div>
+				<input type="number" name="BR_long" title="Longitude" placeholder="Longitude in °" step="any" 
+					value={form?.coordinateCenter.CoordinateSchema.coordinates[0] ?? ''} />
+				<input type="number" name="BR_lat" title="Latitude" placeholder="Latitude in °" step="any" 
+					value={form?.coordinateCenter.CoordinateSchema.coordinates[1] ?? ''} />
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+				<div class="input-group-shim">Radius</div>
+				<input type="number" name="radius" title="Radius" placeholder="Enter the radius of affected locations here." step="any" value={form?.radius ?? ''} />
+				<div class="input-group-shim">km</div>
+			</div>
+
+			<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+				<div class="input-group-shim">Limit</div>
+				<input type="number" name="radius" title="Radius" placeholder="Limit the affected locations here shown in the record to the X nearest locations." step="any" value={form?.radius ?? ''} />
+				<div class="input-group-shim">locations</div>
+			</div>
+
+		{/if} 
 	</article>
 	<button type="button" class="btn btn-sm variant-filled" on:click={() => entryForm.requestSubmit()}
 		>Submit</button
