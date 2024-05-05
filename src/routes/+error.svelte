4d787f56ee9056bd82ad201640c4cd885def245c
Marc <mgc.macaraeg@gmail.com>
2024-05-06 00:31:38 +0800

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex align-middle h-dvh items-center">
+    <div class="w-screen flex flex-row align-middle justify-center">
+        <h1 class="h1">{$page.error?.message}</h1>
+        <button type="button" class="btn variant-filled" on:click={() => goto('/')}>Back to Home</button>
+    </div>
+</div>
